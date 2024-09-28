9565367c5f6ffb41e96033b5182065e800b21046
fernando@abcdataz.com <fernando@abcdataz.com>
2024-09-28 20:06:39 +1200

    added default values to variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,34 +1,39 @@
-variable "project_id" {
-  description = "GCP project ID"
+variable "terraform_sa_key_path" {
+  description = "path to the Terraform service account key JSON file"
   type        = string
+}
+
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+  default     = "semantc-dev"
 }
 
 variable "region" {
-  description = "GCP region"
+  description = "GCP Region"
   type        = string
-}
-
-variable "terraform_sa_key_path" {
-  description = "Path to the Terraform service account key JSON file"
-  type        = string
-}
-
-variable "new_client_id" {
-  description = "Unique identifier for the new client"
-  type        = string
-}
-
-variable "new_client_token" {
-  description = "Token for the new client"
-  type        = string
+  default     = "us-central1"
 }
 
 variable "master_sa_email" {
-  description = "Email of the master service account"
+  description = "Master Service Account Email"
   type        = string
+  default     = "master-sa@semantc-dev.iam.gserviceaccount.com"
 }
 
 variable "data_location" {
   description = "Location for data storage services"
+  type        = string
+  default     = "US"
+}
+
+# Existing variables
+variable "new_client_id" {
+  description = "Unique Client Identifier"
+  type        = string
+}
+
+variable "new_client_token" {
+  description = "Secure Client Token"
   type        = string
 }
