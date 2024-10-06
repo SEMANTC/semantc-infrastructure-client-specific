5fd2b97219bad12b5ede17098026952ae617d084
fernando@abcdataz.com <fernando@abcdataz.com>
2024-10-06 16:15:55 +1300

    added scheduler

--- terraform/modules/cloud_run_jobs/main.tf
+++ terraform/modules/cloud_run_jobs/main.tf
@@ -85,3 +85,41 @@ resource "google_cloud_run_v2_job" "data_transformation_job" {
     ]
   }
 }
+
+resource "google_cloud_scheduler_job" "data_ingestion_scheduler" {
+  name             = "schedule-data-ingestion-${var.new_tenant_id}"
+  description      = "Triggers the data ingestion job every 4 hours"
+  schedule         = "0 */4 * * *"
+  time_zone        = "UTC"
+  attempt_deadline = "320s"
+  region           = var.region
+  project          = var.project_id
+
+  http_target {
+    http_method = "POST"
+    uri         = "https://${var.region}-run.googleapis.com/apis/run.googleapis.com/v1/namespaces/${var.project_id}/jobs/${google_cloud_run_v2_job.data_ingestion_job.name}:run"
+
+    oauth_token {
+      service_account_email = var.master_sa_email
+    }
+  }
+}
+
+# resource "google_cloud_scheduler_job" "data_transformation_scheduler" {
+#   name             = "schedule-data-transformation-${var.new_tenant_id}"
+#   description      = "Triggers the data transformation job every 4 hours"
+#   schedule         = "0 */4 * * *"
+#   time_zone        = "UTC"
+#   attempt_deadline = "320s"
+#   region           = var.region
+#   project          = var.project_id
+
+#   http_target {
+#     http_method = "POST"
+#     uri         = "https://${var.region}-run.googleapis.com/apis/run.googleapis.com/v1/namespaces/${var.project_id}/jobs/${google_cloud_run_v2_job.data_transformation_job.name}:run"
+
+#     oauth_token {
+#       service_account_email = var.master_sa_email
+#     }
+#   }
+# }
